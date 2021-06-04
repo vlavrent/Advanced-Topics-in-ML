280ce73c660e32e043e973e5adbd39c882348a5a
stathism27 <32590853+stathism27@users.noreply.github.com>
2021-06-04 14:06:17 +0300

    added methods and experiments

--- Multi-Label/powerset.py
+++ Multi-Label/powerset.py
@@ -1,10 +1,11 @@
 import scipy
 from scipy.io import arff
-from scipy.io import arff
+from xgboost import XGBClassifier
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from skmultilearn.problem_transform import BinaryRelevance
+
+from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import hamming_loss
@@ -22,8 +23,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import KFold
 import warnings
+import matplotlib
 warnings.filterwarnings("ignore")
-
+print(matplotlib.__version__)
 def clean_data(data):
 
     df = pd.DataFrame(data)
@@ -36,18 +38,22 @@ def clean_data(data):
 
     return X,y
 
-def binary(X_train, X_test, y_train, y_test):
+allClassifiers = ["XGBoost","RandomForest","SVM","LogisticRegression"]
+def chooseClassifier(classifier,X_train,y_train):
+    if(classifier == "XGBoost"):
+        model = LabelPowerset(XGBClassifier(random_state = 0)).fit(X_train,y_train)
+    elif(classifier == "RandomForest"):
+        model = LabelPowerset(RandomForestClassifier(n_estimators = 1000, criterion = 'entropy', random_state = 0)).fit(X_train,y_train)
+    elif(classifier == "SVM"):
+        model = LabelPowerset(LinearSVC(random_state=0)).fit(X_train,y_train)
+    elif(classifier == "LogisticRegression"):
+        model = LabelPowerset(LogisticRegression(random_state=0)).fit(X_train,y_train)
+    return model
 
-    print("Binary Relevance")
-    model = BinaryRelevance(classifier=SVC(), require_dense=[True, True]).fit(X_train, y_train)
-    print("Hamming: {}".format(hamming_loss(y_test, model.predict(X_test))))
-    print("Accuracy: {}".format(accuracy_score(y_test, model.predict(X_test))))
-    print("\n")
-
-def powerset(X_train, X_test, y_train, y_test):
+def powerset(X_train, X_test, y_train, y_test,classifier):
 
     print("Label Powerset")
-    model = LabelPowerset(SVC()).fit(X_train,y_train)
+    model = chooseClassifier(classifier,X_train,y_train)
     y_pred = model.predict(X_test)
 
     hamming = hamming_loss(y_test, y_pred)
@@ -122,39 +128,39 @@ rankingloss_arr = []
 
 
 
-#all_arr = [hamming_arr ,subset_accuracy_arr ,recall_arr ,precision_arr ,f1_arr ,coverage_arr ,aps_arr,rankingloss_arr]
 all_arr = [hamming_arr ,subset_accuracy_arr ,recall_arr ,precision_arr ,f1_arr ]
-for train_index, test_index in kf.split(X):
-    #print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
-    hamming, subset_accuracy, recall, precision, f1, coverage, aps, rankingloss = powerset(X_train, X_test, y_train, y_test)
-    hamming_arr.append(hamming)
-    subset_accuracy_arr.append(subset_accuracy)
-    recall_arr.append(recall)
-    precision_arr.append(precision)
-    f1_arr.append(f1)
-    coverage_arr.append(coverage)
-    aps_arr.append(aps)
-    rankingloss_arr.append(rankingloss)
+for c in allClassifiers:
+    for train_index, test_index in kf.split(X):
+        #print("TRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        hamming, subset_accuracy, recall, precision, f1, coverage, aps, rankingloss = powerset(X_train, X_test, y_train, y_test,c)
+        hamming_arr.append(hamming)
+        subset_accuracy_arr.append(subset_accuracy)
+        recall_arr.append(recall)
+        precision_arr.append(precision)
+        f1_arr.append(f1)
+        coverage_arr.append(coverage)
+        aps_arr.append(aps)
+        rankingloss_arr.append(rankingloss)
 
-finalscores = []
-for lst in all_arr:
-    finalscores.append(np.array(lst).mean())
+    finalscores = []
+    for lst in all_arr:
+        finalscores.append(np.array(lst).mean())
 
-#objects = ('Hamming Loss', 'Subset Accuracy', 'Recall', 'Precision', 'f1', 'Coverage','Average Precision','Ranking Loss')
-objects = ('Hamming Loss', 'Subset Accuracy', 'Recall', 'Precision', 'f1')
-y_pos = np.arange(len(objects))
+    #objects = ('Hamming Loss', 'Subset Accuracy', 'Recall', 'Precision', 'f1', 'Coverage','Average Precision','Ranking Loss')
+    objects = ('Hamming Loss', 'Subset Accuracy', 'Recall', 'Precision', 'f1')
+    y_pos = np.arange(len(objects))
 
-width = 0.35
-fig, ax = plt.subplots()
-b = ax.bar(y_pos, finalscores, color='brown',align='center')
-ax.set_xticks(y_pos)
-ax.set_xticklabels(objects)
-ax.set_ylabel('Score')
-ax.bar_label(b, fmt='%.3f')
-plt.title('Metrics')
-plt.show()
+    width = 0.35
+    fig, ax = plt.subplots()
+    b = ax.bar(y_pos, finalscores, color='brown',align='center')
+    ax.set_xticks(y_pos)
+    ax.set_xticklabels(objects)
+    ax.set_ylabel('Score')
+    ax.bar_label(b, fmt='%.3f')
+    plt.title('Metrics of '+c)
+    plt.savefig("POWERSET/"+c+".png", transparent=True,dpi=200)
 
 
 
